13e33a69d2fb07783c796e484c037510557c4935
tuanemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2025-09-12 21:56:27 +0700

    update register function

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -17,6 +17,8 @@ const Input = React.forwardRef(
       inputStyle = "",
       // @ts-ignore
       error,
+      // @ts-ignore
+      options = [],
       ...rest
     },
     ref,
@@ -26,14 +28,29 @@ const Input = React.forwardRef(
         <label htmlFor={id} className={`mb-3 block ${labelStyle}`}>
           {label}
         </label>
-        <input
-          className={`px-3 py-2 outline-none ${inputStyle}`}
-          type={type}
-          id={id}
-          placeholder={placeholder}
-          ref={ref}
-          {...rest}
-        />
+        {type === "select" ? (
+          <select
+            className={`px-3 py-2 outline-none ${inputStyle}`}
+            id={id}
+            {...rest}
+            ref={ref}
+          >
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            className={`px-3 py-2 outline-none ${inputStyle}`}
+            type={type}
+            id={id}
+            placeholder={placeholder}
+            ref={ref}
+            {...rest}
+          />
+        )}
         {error && <p className="mt-1 text-sm text-red-600">{error}</p>}
       </div>
     );
--- src/pages/Passport.jsx
+++ src/pages/Passport.jsx
@@ -56,169 +56,181 @@ const Passport = () => {
   return (
     <>
       {user ? (
-        <div className="flex-1 bg-[url(/background.svg)] bg-cover bg-center">
-          <div className="container mx-auto py-10">
-            <div className="flex justify-between">
-              <MucLuc />
-              <div className="flex h-[650px] w-[calc((100%*3/4)-20px)] items-center justify-center">
-                <Swiper
-                  slidesPerView={1}
-                  centeredSlides={false}
-                  slidesPerGroupSkip={1}
-                  grabCursor={true}
-                  keyboard={{
-                    enabled: true,
-                  }}
-                  breakpoints={{
-                    769: {
-                      slidesPerView: 2,
-                      slidesPerGroup: 2,
-                    },
-                  }}
-                  scrollbar={true}
-                  // pagination={{
-                  //   clickable: true,
-                  // }}
-                  modules={[Keyboard, Scrollbar, Navigation, Pagination]}
-                  className="passportSwiper"
-                >
-                  <SwiperSlide>
-                    <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] py-7">
-                      <div className="flex h-full flex-col items-center justify-between text-3xl font-bold text-[#fbf9d7]">
-                        <h2>HỘ CHIẾU SỐ</h2>
-                        <FaEarthAmericas className="text-9xl" />
-                        <div>TRƯỜNG THCS VÂN ĐỒN</div>
+        user.type === "Người kết nối" ||
+        user.type === "Người sáng tạo" ||
+        user.type === "Người quan sát" ? (
+          <div className="flex-1 bg-[url(/background.svg)] bg-cover bg-center">
+            <div className="container mx-auto py-10">
+              <div className="flex justify-between">
+                <MucLuc />
+                <div className="flex h-[650px] w-[calc((100%*3/4)-20px)] items-center justify-center">
+                  <Swiper
+                    slidesPerView={1}
+                    centeredSlides={false}
+                    slidesPerGroupSkip={1}
+                    grabCursor={true}
+                    keyboard={{
+                      enabled: true,
+                    }}
+                    breakpoints={{
+                      769: {
+                        slidesPerView: 2,
+                        slidesPerGroup: 2,
+                      },
+                    }}
+                    scrollbar={true}
+                    // pagination={{
+                    //   clickable: true,
+                    // }}
+                    modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+                    className="passportSwiper"
+                  >
+                    <SwiperSlide>
+                      <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] py-7">
+                        <div className="flex h-full flex-col items-center justify-between text-3xl font-bold text-[#fbf9d7]">
+                          <h2>HỘ CHIẾU SỐ</h2>
+                          <FaEarthAmericas className="text-9xl" />
+                          <div>TRƯỜNG THCS VÂN ĐỒN</div>
+                        </div>
                       </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
-                      <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
-                        <h2 className="text-2xl font-bold">
-                          THÔNG TIN TÀI KHOẢN
-                        </h2>
-                        <div>
-                          <div className="flex flex-col items-center">
-                            <div className="aspect-square w-[100px]">
-                              <img
-                                src={
-                                  user
-                                    ? `${apiUrl}user/get-avatar/${user.username}`
-                                    : "https://avatar.iran.liara.run/public/33"
-                                }
-                                alt="avatar"
-                                className="h-full w-full rounded-full object-cover shadow-lg"
-                              />
-                            </div>
-                            <div className="mt-2 text-center">
-                              <div className="font-bold">{user.name}</div>
-                              <div className="text-sm text-gray-500">
-                                Học sinh
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
+                        <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
+                          <h2 className="text-2xl font-bold">
+                            THÔNG TIN TÀI KHOẢN
+                          </h2>
+                          <div>
+                            <div className="flex flex-col items-center">
+                              <div className="aspect-square w-[100px]">
+                                <img
+                                  src={
+                                    user
+                                      ? `${apiUrl}user/get-avatar/${user.username}`
+                                      : "https://avatar.iran.liara.run/public/33"
+                                  }
+                                  alt="avatar"
+                                  className="h-full w-full rounded-full object-cover shadow-lg"
+                                />
                               </div>
-                            </div>
-                          </div>
-                          <div className="mt-5">
-                            <div className="flex flex-col gap-2">
-                              <div className="flex justify-between">
-                                <div className="text-lg text-gray-500">
-                                  Ngày tham gia:
+                              <div className="mt-2 text-center">
+                                <div className="font-bold">{user.name}</div>
+                                <div className="text-sm text-gray-500">
+                                  Học sinh
                                 </div>
-                                <div className="font-bold">11/09/2025</div>
-                              </div>
-                              <div className="flex justify-between">
-                                <div className="text-lg text-gray-500">
-                                  Email:
-                                </div>
-                                <div className="font-bold">{user.email}</div>
-                              </div>
-                              <div className="flex justify-between">
-                                <div className="text-lg text-gray-500">
-                                  Giới tính:
-                                </div>
-                                <div className="font-bold capitalize">
-                                  {user.sex}
-                                </div>
-                              </div>
-                              <div className="flex justify-between">
-                                <div className="text-lg text-gray-500">
-                                  Sở thích:
-                                </div>
-                                <div className="font-bold">{user.hobby}</div>
                               </div>
                             </div>
+                            <div className="mt-5">
+                              <div className="flex flex-col gap-2">
+                                <div className="flex justify-between">
+                                  <div className="text-lg text-gray-500">
+                                    Ngày tham gia:
+                                  </div>
+                                  <div className="font-bold">11/09/2025</div>
+                                </div>
+                                <div className="flex justify-between">
+                                  <div className="text-lg text-gray-500">
+                                    Email:
+                                  </div>
+                                  <div className="font-bold">{user.email}</div>
+                                </div>
+                                <div className="flex justify-between">
+                                  <div className="text-lg text-gray-500">
+                                    Giới tính:
+                                  </div>
+                                  <div className="font-bold capitalize">
+                                    {user.sex}
+                                  </div>
+                                </div>
+                                <div className="flex justify-between">
+                                  <div className="text-lg text-gray-500">
+                                    Sở thích:
+                                  </div>
+                                  <div className="font-bold">{user.hobby}</div>
+                                </div>
+                              </div>
+                            </div>
                           </div>
+                          <div>Trang 1</div>
                         </div>
-                        <div>Trang 1</div>
                       </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
-                      <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
-                        <h2 className="text-2xl font-bold">
-                          ĐỊNH DANH BẢN THÂN
-                        </h2>
-                        <div className="flex h-full flex-col items-center justify-center gap-5">
-                          <h2 className="font-semibold">BẠN CHÍNH LÀ</h2>
-                          <div className="text-3xl font-bold text-[#FF9C33] italic">
-                            {user.type}
-                          </div>
-                          <div className="text-center text-sm">
-                            LƯỚT QUA TRANG KẾ ĐỂ XEM VIDEO VÀ CẨM NANG
-                          </div>
-                        </div>
-                        <div>Trang 2</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
-                      <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
-                        <h2 className="text-2xl font-bold">VIDEO</h2>
-                        <div className="">
-                          <div className="mb-5 text-xl">
-                            Bạn hãy xem video để hiểu rõ hơn về tính cách bản
-                            thân mình nhé
-                          </div>
-                          <iframe
-                            className="aspect-video w-full rounded-xl shadow-lg"
-                            src={videoLink}
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerPolicy="strict-origin-when-cross-origin"
-                            allowFullScreen
-                          ></iframe>
-                        </div>
-                        <div>Trang 3</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
-                      <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
-                        <h2 className="mb-20 text-2xl font-bold">CẨM NANG</h2>
-                        <div className="flex flex-col items-center text-center text-lg">
-                          <a className="mb-8" href={pdfLink}>
-                            <IoDocumentTextSharp className="text-8xl hover:text-blue-500" />
-                          </a>
-                          <div className="text-xl">
-                            Bạn hãy bấm vào icon để tải cẩm nang về{" "}
-                            <span className="font-bold text-[#FF9C33] italic">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
+                        <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
+                          <h2 className="text-2xl font-bold">
+                            ĐỊNH DANH BẢN THÂN
+                          </h2>
+                          <div className="flex h-full flex-col items-center justify-center gap-5">
+                            <h2 className="font-semibold">BẠN CHÍNH LÀ</h2>
+                            <div className="text-3xl font-bold text-[#FF9C33] italic">
                               {user.type}
-                            </span>{" "}
-                            nhé
+                            </div>
+                            <div className="text-center text-sm">
+                              LƯỚT QUA TRANG KẾ ĐỂ XEM VIDEO VÀ CẨM NANG
+                            </div>
                           </div>
+                          <div>Trang 2</div>
                         </div>
-                        <div>Trang 4</div>
                       </div>
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
+                        <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
+                          <h2 className="text-2xl font-bold">VIDEO</h2>
+                          <div className="">
+                            <div className="mb-5 text-xl">
+                              Bạn hãy xem video để hiểu rõ hơn về tính cách bản
+                              thân mình nhé
+                            </div>
+                            <iframe
+                              className="aspect-video w-full rounded-xl shadow-lg"
+                              src={videoLink}
+                              title="YouTube video player"
+                              frameBorder="0"
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                              referrerPolicy="strict-origin-when-cross-origin"
+                              allowFullScreen
+                            ></iframe>
+                          </div>
+                          <div>Trang 3</div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <div className="flex h-full w-full items-center justify-center rounded-2xl bg-[#08083C] p-7">
+                        <div className="flex h-full w-full flex-col justify-between rounded-2xl bg-[#fbf9d7] p-5">
+                          <h2 className="mb-20 text-2xl font-bold">CẨM NANG</h2>
+                          <div className="flex flex-col items-center text-center text-lg">
+                            <a className="mb-8" href={pdfLink}>
+                              <IoDocumentTextSharp className="text-8xl hover:text-blue-500" />
+                            </a>
+                            <div className="text-xl">
+                              Bạn hãy bấm vào icon để tải cẩm nang về{" "}
+                              <span className="font-bold text-[#FF9C33] italic">
+                                {user.type}
+                              </span>{" "}
+                              nhé
+                            </div>
+                          </div>
+                          <div>Trang 4</div>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex h-[80vh] flex-col items-center justify-center bg-[url(/background.svg)] bg-cover bg-center">
+            <div className="flex flex-col rounded-2xl bg-white/70 p-10 shadow-lg">
+              <h2 className="mb-5 text-3xl font-bold">
+                Bạn cần phải làm quiz để thông tin được hiển thị
+              </h2>
+            </div>
+          </div>
+        )
       ) : (
         <div className="flex h-[80vh] flex-col items-center justify-center bg-[url(/background.svg)] bg-cover bg-center">
           <div className="flex flex-col rounded-2xl bg-white/70 p-10 shadow-lg">
